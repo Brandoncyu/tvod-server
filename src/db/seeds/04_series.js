143143026ecece9ceeb3f65f3fe635583bc884ed
Brandon <brandoncyu@gmail.com>
2018-09-27 10:22:47 -0700

    added new image column to series table

--- src/db/seeds/04_series.js
+++ src/db/seeds/04_series.js
@@ -5,6 +5,7 @@ exports.seed = knex => {
       user_id: 1,
       tv_id: 618,
       tv_name: 'Better Call Saul',
+      image: "http://static.tvmaze.com/uploads/images/medium_portrait/163/407938.jpg",
       watched: true,
       favorite: true
     }, {
@@ -12,6 +13,7 @@ exports.seed = knex => {
       user_id: 1,
       tv_id: 82,
       tv_name: 'Game of Thrones',
+      image: "http://static.tvmaze.com/uploads/images/medium_portrait/143/359013.jpg",
       watched: true,
       favorite: true
     }, {
@@ -19,6 +21,7 @@ exports.seed = knex => {
       user_id: 1,
       tv_id: 304,
       tv_name: 'The Venture Bros.',
+      image: "http://static.tvmaze.com/uploads/images/medium_portrait/2/5028.jpg",
       watched: true,
       favorite: true
     }, {
@@ -26,6 +29,7 @@ exports.seed = knex => {
       user_id: 1,
       tv_id: 347,
       tv_name: "It's Always Sunny in Philadelphia",
+      image: "http://static.tvmaze.com/uploads/images/medium_portrait/44/110392.jpg",
       watched: true,
       favorite: true
     }, {
@@ -33,6 +37,7 @@ exports.seed = knex => {
       user_id: 1,
       tv_id: 123,
       tv_name: 'Lost',
+      image: "http://static.tvmaze.com/uploads/images/medium_portrait/0/1389.jpg",
       watched: true,
       favorite: true
     }

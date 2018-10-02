a346fd2f9c8fe9278a2c6628abb1706098bda809
Brandon <brandoncyu@gmail.com>
2018-10-02 08:58:23 -0700

    added new seeds

--- src/db/seeds/16_users_following.js
+++ src/db/seeds/16_users_following.js
@@ -6,9 +6,28 @@ exports.seed = knex => {
             user_id: 1,
             follow_id: 2
           }, {
+            user_id: 1,
+            follow_id: 7
+          }, {
+            user_id: 1,
+            follow_id: 8
+          }, {
+            user_id: 1,
+            follow_id: 11
+          }, {
             user_id: 2,
             follow_id: 1
+          }, {
+            user_id: 4,
+            follow_id: 1
+          }, {
+            user_id: 5,
+            follow_id: 1
+          }, {
+            user_id: 10,
+            follow_id: 1
           }
+
         ])
         .then(() => {
           return knex.raw(`SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM ${table}));`)
